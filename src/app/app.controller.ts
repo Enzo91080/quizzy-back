deb5997949731246541bc9ec9ed0c55254adef79
Enzo AIME <enzo.aime91@gmail.com>
2025-02-17 12:11:28 +0100

    first commit

---
+++ src/app/app.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('')
+  getData() {
+    return this.appService.getData();
+  }
+
+  @Post('message')
+  postData(@Body() message: string) {
+    console.log(message);
+    return message;
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { FirebaseModule } from 'nestjs-firebase';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PingModule } from './ping/ping.module';
+import { PokemonsController } from './pokemon/pokemons.controller';
+import { UsersController } from './users/users.controller';
+import { AuthModule } from './modules/auth/auth.module';
+import { AuthMiddleware } from './modules/auth/auth.middleware';
+
+@Module({
+  imports: [
+    PingModule,
+    FirebaseModule.forRoot({
+      googleApplicationCredential: 'src/assets/quizzy-back-firebase-key.json',
+    }),
+    AuthModule,
+  ],
+  controllers: [AppController, PokemonsController, UsersController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  public configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
