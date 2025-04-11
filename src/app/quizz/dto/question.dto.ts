145729094917f7761155feca0daf868509b8736c
Maxime Lemesle <maxime.lemesle7@gmail.com>
2025-04-11 10:59:52 +0200

    fix(QuestionDto): remove arrayMinSize

--- src/app/quizz/dto/question.dto.ts
+++ src/app/quizz/dto/question.dto.ts
@@ -1,19 +1,18 @@
 // --- question.dto.ts ---
 import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsNotEmpty, IsString, ValidateNested, ArrayMinSize } from 'class-validator';
+import { IsArray, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 import { AnswerDto } from './create-question.dto';
 
 export class QuestionDto {
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    title: string;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  title: string;
 
-    @ApiProperty({ type: [AnswerDto] })
-    @IsArray()
-    @ArrayMinSize(2)
-    @ValidateNested({ each: true })
-    @Type(() => AnswerDto)
-    answers: AnswerDto[];
+  @ApiProperty({ type: [AnswerDto] })
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => AnswerDto)
+  answers: AnswerDto[];
 }
